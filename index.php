5bc8b93b8a1005ba2be2550b23974ca49b184bec
Masaya Kijima <Masaya@Masaya-no-MacBook-Air.local>
2020-01-26 18:19:23 +0900

    3rd title modified

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="ja">
 <head>
  <meta charset="utf-8">
- <title>ウェブストエイト改改</title>
+ <title>怪怪ウェブストエイト</title>
  <meta name="viewport" content="width=device-width, initial-scale=1">
  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/mystyle.css">
